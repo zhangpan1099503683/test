74ca2314c8aa48822e15b3daab020aca2c7db912
zhangpan <1099503683@qq.com>
2016-07-30 15:39:23 +0800

    MyPlayground

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -27,9 +27,45 @@ else
 }
 
 
+var s=1;
+for var i=0; i<10; i=i+1
+{
+    s=s+i;
+}
+print(s);
+
+
+var arr=[1,2,3]
+print(arr)
+
+arr.append(2)
+arr.removeAtIndex(2)
+
+
+
+var brr=[3,6,8,12,15,21,2]
+
+
+for var ii=0;ii<7;ii=ii+1
+
+{
+    for var jj=ii+1;jj<7;jj=jj+1
+    {
+    if brr[ii]<brr[jj]
+    {
+        var temp = brr[ii]
+        brr[ii] = brr[jj]
+        brr[ii]=temp
+    }
+        
+    }
+}
+
+print(brr);
 
 
 
 
+	
 
         
